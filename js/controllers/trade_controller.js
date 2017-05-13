b9e78426872da7c8c17b3d5c615958b69a807e08
tt <ttwang.303@gmail.com>
2017-05-13 01:18:08 -0400

    disable form submission if invalid

--- js/controllers/trade_controller.js
+++ js/controllers/trade_controller.js
@@ -9,8 +9,7 @@ Fideligard.controller("TradeCtrl",
       price: $stateParams.price,
       symbol: $stateParams.symbol,
       quantity: 10,
-      buy: true,
-
+      type: true,
     };
 
     // from trade service
@@ -20,12 +19,10 @@ Fideligard.controller("TradeCtrl",
       return !isNaN($scope.newTrade.quantity) ? ($scope.newTrade.quantity * $scope.newTrade.price) : '--';
     }
 
-    $scope.orderStatus = $scope.cost < $scope.cash ? true : false;
-
-    $scope.checkValidity = function() {
-      $scope.cost < $scope.cash ? true : false;
+    $scope.orderStatus = function() {
       // cost is less than cash on hand
       // TO DO: can't sell stocks not in portfolio
+      return $scope.cost() < $scope.cash ? true : false;
     }
 
     $scope.submitTrade = function() {
