0db8491f364a9ec387ff4ce1c1736cf339117332
tt <ttwang.303@gmail.com>
2017-05-14 15:54:35 -0400

    welcome screen conditional on trades instead of positions

--- js/controllers/portfolio_controller.js
+++ js/controllers/portfolio_controller.js
@@ -2,6 +2,8 @@ Fideligard.controller("PortfolioCtrl",
   ["$scope", "TradeService", "DateService", "PortfolioService",
   function($scope, TradeService, DateService, PortfolioService) {
 
+    $scope.trades = TradeService.getTrades();
+
     $scope.date = DateService.stringFormat();
 
     $scope.positions = PortfolioService.getPositions();
@@ -10,6 +12,7 @@ Fideligard.controller("PortfolioCtrl",
 
     // update portfolio table with current date selections
     $scope.$on('change.date', function(event) {
+      $scope.trades = TradeService.getTrades();
       $scope.date = DateService.hyphenFormat();
       $scope.positions = PortfolioService.getPositions();
       $scope.overview = PortfolioService.getOverview();
